4ee47fce64c47331e0c60c07881d71016fe1c75b
phpfaucet <developer@phpfaucet.org>
2015-04-08 08:16:15 -0700

    Prevent Referral Fraud

--- header.php
+++ header.php
@@ -4,8 +4,20 @@ require_once "maincore.php";
 
 
 if(isset($_GET['r']) &&  $_GET['r']>0){
+
+		//If IP referral not equal with user IP
+		
+	$ruid = $_GET['r'];
+	$prepare=$db->mysqli->prepare("select ip from tbl_user where user_id=?");
+	$prepare->bind_param('i',$ruid);
+	$prepare->execute();
+	$prepare->bind_result($refip);
+	$prepare->fetch();
 	
+	if ( $ip != $refip ) {
 	setcookie('r', trim($_GET['r']), time() + 60 * 60 * 24 * 30);
+	}
+	$prepare->close();
 	
 }
 
